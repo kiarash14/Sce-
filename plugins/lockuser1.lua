8d53f43e468706919052980e78f9b30e1e73fd93
kiarash14 <omidbakhsh22@gmail.com>
2016-03-30 05:16:40 -0700

    creating for Kiarash باگ داره

---
+++ plugins/lockuser1.lua
@@ -0,0 +1,16 @@
+ if data[tostring(msg.to.id)] then
+       if data[tostring(msg.to.id)]['settings'] then
+         if data[tostring(msg.to.id)]['settings']['lock_join'] then
+           bots_protection = data[tostring(msg.to.id)]['settings']['lock_join']
+          end
+        end
+      end
+    if msg.action.user.username ~= nil then
+      if string.sub(msg.action.user.username:lower(), -3) == 'online' and not is_momod(msg) and bots_protection == "yes" then --- Will kick bots added by normal users
+          local print_name = user_print_name(msg.from):gsub("‮", "")
+		  local name = print_name:gsub("_", "")
+          savelog(msg.to.id, name.." ["..msg.from.id.."] added a bot > @".. msg.action.user.username)-- Save to logs
+          kick_user(msg.action.user.id, msg.to.id)
+      end
+    end
+  end
